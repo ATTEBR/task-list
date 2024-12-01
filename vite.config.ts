9186122c6a190ac055bc3c7e33ce2f1a156848ac
ATTEBR <aman@uppermost.cc>
2024-12-01 07:59:19 -0500

    Add homepage field to package.json for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/task-list/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
 })
